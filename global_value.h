160f5bec7eac82bd646d4bf0cd5f3b5f2f3eb156
Shin DongWoon <shindwn@hstelnet.com>
2024-08-04 14:08:08 +0900

    Ver 0.8.k 20240804 	1. 0.8.j 버전에서 추가 빠진 내용 추가 	2. 초인종 관련 버그 수정 	 - 클립 녹화가 종료되지 않은 시점에서 초인종 녹화를 시작하면 초인종 파일이 정상적이지 않은 버그 	 - 클립 녹화와 초인종 녹화를 정확히 분리하여 문제 수정

--- global_value.h
+++ global_value.h
@@ -13,7 +13,7 @@ extern "C" {
 #ifndef __PHILL_REQ__
 	#define MAJOR_VER	"0"
 	#define MINOR_VER	"8"
-	#define CAHR_VER	"j"
+	#define CAHR_VER	"k"
 #else
 	#define MAJOR_VER	"0"
 	#define MINOR_VER	"9"	
